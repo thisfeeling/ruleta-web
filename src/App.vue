360282d5103ce383aa660bbac202f12006364374
thisfeeling <murksopps@gmail.com>
2025-12-19 23:36:16 -0500

    feat: remove TresJS integration and associated components from the application

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,8 @@
 <script setup lang="ts">
-import { TresCanvas } from '@tresjs/core'
-import FirstExperience from '@/modules/try/FirstExperience.vue'
-
 import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <TresCanvas
-    clear-color="#82DBC5"
-    window-size
-  >
-    <FirstExperience />
-  </TresCanvas>
-
   <RouterView />
 </template>
 
